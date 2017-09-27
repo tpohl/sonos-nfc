1d2996373e4a2b09751dd7c0d35b74a0a240f6d2
Thorsten Pohl <thorsten@pohl.rocks>
2017-09-27 21:42:09 +0000

    Working write and read, but destroyed two cards.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 require('dotenv').config();
 console.log('Hello World');
 
@@ -6,13 +7,72 @@ var request = require('request');
 var SONOS_API_URL = process.env.SONOS_API_URL;
 var ROOM = process.env.SONOS_ROOM;
 
-function playSonos(itemName, type = 'playlist){
+function playSonos(itemName, type){
   var url = SONOS_API_URL+ROOM+'/'+type+'/'+itemName;
   request(url, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
-  }
+  });
 }
 
-playSonos('Giraffenaffen');
+
+
+const mfrc522 = require("mfrc522-rpi");
+//# Init Chip
+mfrc522.init();
+
+//# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+console.log("scanning...");
+console.log("Please put chip or keycard in the antenna inductive zone!");
+console.log("Press Ctrl-C to stop.");
+
+setInterval(function(){
+
+    //# Scan for cards
+    let response = mfrc522.findCard();
+    if (!response.status) {
+        return;
+    }
+    console.log("Card detected, CardType: " + response.bitSize);
+
+    //# Get the UID of the card
+    response = mfrc522.getUid();
+    if (!response.status) {
+        console.log("UID Scan Error");
+        return;
+    }
+    //# If we have the UID, continue
+    const uid = response.data;
+    console.log("Card read UID: %s %s %s %s", uid[0].toString(16), uid[1].toString(16), uid[2].toString(16), uid[3].toString(16));
+
+    //# Select the scanned card
+    const memoryCapacity = mfrc522.selectCard(uid);
+    console.log("Card Memory Capacity: " + memoryCapacity);
+
+    //# This is the default key for authentication
+    const key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
+
+    var buffers = [];
+for (let i = 4; i < 7; i++) {
+        if (mfrc522.authenticate(i, key, uid)) {
+            buffers.push(new Buffer(mfrc522.getDataForBlock(i)));
+            console.log("Block: "+i+" Data: ", buffers.length);
+        } else {
+            console.log("Authentication Error");
+//            break;
+        }
+    }
+
+    var totalBuffer = Buffer.concat(buffers);
+    var payload = totalBuffer.toString();
+    console.log('payload', payload);
+
+    var object =  JSON.parse(payload);
+    console.log('Object', object);
+    //# Stop
+    mfrc522.stopCrypto();
+
+}, 500);
+
+//playSonos('Giraffenaffen');
---
+++ write.js
@@ -0,0 +1,69 @@
+"use strict";
+const mfrc522 = require("mfrc522-rpi");
+//# Init Chip
+mfrc522.init();
+
+//# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+console.log("scanning...");
+console.log("Please put chip or keycard in the antenna inductive zone!");
+console.log("Press Ctrl-C to stop.");
+
+var payload = {type:'playlist',value:'Giraffenaffen'};
+
+setInterval(function(){
+
+    //# Scan for cards
+    let response = mfrc522.findCard();
+    if (!response.status) {
+        return;
+    }
+    console.log("Card detected, CardType: " + response.bitSize);
+
+    //# Get the UID of the card
+    response = mfrc522.getUid();
+    if (!response.status) {
+        console.log("UID Scan Error");
+        return;
+    }
+    //# If we have the UID, continue
+    const uid = response.data;
+    console.log("Card read UID: %s %s %s %s", uid[0].toString(16), uid[1].toString(16), uid[2].toString(16), uid[3].toString(16));
+
+    //# Select the scanned card
+    const memoryCapacity = mfrc522.selectCard(uid);
+    console.log("Card Memory Capacity: " + memoryCapacity);
+
+    //# This is the default key for authentication
+    const key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF];
+
+    var buffer = new Buffer(3*16)
+    buffer.fill(' ', 0);;
+    buffer.write(JSON.stringify(payload));
+
+    const trailer = [];
+    var i = 0;
+    for (let block = 4; block < 8; block++) {
+    //# Authenticate on Block  with key and uid
+    if (mfrc522.authenticate(block, key, uid)) {
+        if (block % 4 < 3) {
+        console.log('Writing payload ', i, ' to block', block);
+        var dataToWrite = [buffer[i*16], buffer[i*16+1],buffer[i*16+2], buffer[i*16+3], buffer[i*16+4], buffer[i*16+5], buffer[i*16+6], buffer[i*16+7], buffer[i*16+8],
+                           buffer[i*16+9], buffer[i*16+10], buffer[i*16+11], buffer[i*16+12], buffer[i*16+13], buffer[i*16+14], buffer[i*16+15]];
+	mfrc522.writeDataToBlock(block, dataToWrite);
+        console.log('WROTE BLOCK', block, dataToWrite);
+        i=i+1;
+	} else {
+          console.log('a trailer block : skipping');
+	}
+    } else {
+     console.log('Auth Error');
+     return;
+    }
+ 
+    }
+    mfrc522.stopCrypto();
+
+    console.log("finished successfully!");
+
+
+}, 500);
